8a777fd934b6a637eb66605b7a23f2e943979d2b
BILLMURRAYY <nitipattime@gmail.com>
2022-12-01 01:13:27 +0700

    props

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,11 +4,44 @@ import CartItems from './CartItems'
 const test = () => {
     return "bill"
 }
+
+const product = [
+    {
+        imgUrl: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204',
+        name:'IPhone',
+        price: '35000',
+        qty:'1'
+    },
+    {
+        imgUrl: 'https://images-americanas.b2w.io/produtos/01/00/img/5884147/1/5884147146_1GG.jpg',
+        name:'IPhone',
+        price: '35000',
+        qty:'2'
+    },
+    {
+        imgUrl: 'https://prod-api.mediaexpert.pl/api/images/gallery_500_500/thumbnails/images/41/4117906/Smartfon-APPLE-Polnoc-1.jpg',
+        name:'IPhone',
+        price: '35000',
+        qty:'3'
+    },
+]
+
 const Cart = () => {
   return (
-    <div>
-      <CartItems name="banana" price="30" qty="2" />
-      <CartItems name={test()} price="40" qty="4" />
+      <div className='container'>
+        {/* <CartItems name="banana" price="30" qty="2" />
+        <CartItems name={test()} price="40" qty="4" /> */}
+        <div className="row">
+            {
+                product.map((product, index)=>{
+                    return (
+                        <div className="col-md-4 my-3" key={index}>
+                            <CartItems imgurl={product.imgUrl} name={product.name} price={product.price} qty={product.qty} />
+                        </div>
+                    )
+                })
+            }   
+        </div>
     </div>
   )
 }
--- src/pages/CartItems.jsx
+++ src/pages/CartItems.jsx
@@ -1,10 +1,22 @@
 const CartItems = (props) => {
+
+    // console.log(props)
   return (
-    <div className="card">
-      <p>Name : {props.name}</p>
-      <p>Price : {props.price}</p>
-      <p>Qty : {props.qty}</p>
+    // <div className="card">
+    //   <p>Name : {props.name}</p>
+    //   <p>Price : {props.price}</p>
+    //   <p>Qty : {props.qty}</p>
+    // </div>
+    <div className="card" style={{width: '18rem'}}>
+        <img src={props.imgurl} className="card-img-top" alt="..." />
+        <div className="card-body">
+            <h5 className="card-title">{props.name}</h5>
+            <p className="card-text">{props.price} THB</p>
+            <p className="card-text">{props.qty} Items</p>
+            
+        </div>
     </div>
+
   )
 }
 
